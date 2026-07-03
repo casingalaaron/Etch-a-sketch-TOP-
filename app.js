297f93d4d6e621451d32d5a44d6db325757e3608
casingalaaron <casingalaaron04@gmail.com>
2026-07-03 14:14:43 +0800

    Added most of it's features
    - Function to create a grid
    
    - Function to hover grids and change it's background-color randmomly

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-console.log("Hello HTML")
+function createDiv(){
+    const body = document.querySelector('body')
+    const container = document.createElement("div")
+    container.className = "container"
+    
+    body.appendChild(container)
+    for(let i=0; i <16; i++){
+        const rowContainer = document.createElement("div")
+        container.appendChild(rowContainer)
+        rowContainer.className = "container row-container"
+
+        for(let j=0; j<16; j++){
+            const gridBox = document.createElement('div')
+            gridBox.className = "grid"
+            rowContainer.appendChild(gridBox)
+        }
+    }
+}
+function hoverGrid(){
+    const gridBoxes = document.querySelectorAll('.grid')
+
+    gridBoxes.forEach((grid) => {
+        grid.addEventListener('mouseenter', (event) => {
+            grid.style.backgroundColor = changeColor()
+        })
+    })
+
+}
+
+function changeColor(){
+    return "#" + Math.round(Math.random() * 1000000)
+}
+
+createDiv()
+hoverGrid()
