1d982fcfa8e638a8f0a8916270cf446c3154d27a
casingalaaron <casingalaaron04@gmail.com>
2026-07-03 19:17:46 +0800

    added button for modifying Grid size

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-function createDiv(){
+function createGrid(size=16){
     const body = document.querySelector('body')
     const container = document.createElement("div")
     container.className = "container"
     
     body.appendChild(container)
-    for(let i=0; i <16; i++){
+    for(let i=0; i <size; i++){
         const rowContainer = document.createElement("div")
         container.appendChild(rowContainer)
         rowContainer.className = "container row-container"
 
-        for(let j=0; j<16; j++){
+        for(let j=0; j<size; j++){
             const gridBox = document.createElement('div')
             gridBox.className = "grid"
             rowContainer.appendChild(gridBox)
         }
     }
+    const gridCount = document.querySelectorAll('.grid').length
+    console.log(gridCount)
 }
 function hoverGrid(){
     const gridBoxes = document.querySelectorAll('.grid')
@@ -31,5 +33,29 @@ function changeColor(){
     return "#" + Math.round(Math.random() * 1000000)
 }
 
-createDiv()
+function enterButton(){
+    const button = document.createElement('button')
+    const body = document.querySelector('body')
+
+    button.textContent = "Change Size"
+    button.className = "changeSizeBtn"
+    body.appendChild(button)
+    button.addEventListener('click', () => {
+        const size = prompt("Maximum size is 100x100\n\nEnter size:")
+        if(size){
+            console.log(size)
+            const container = document.querySelector(".container")
+            const rowContainer = document.querySelectorAll(".row-container")
+            container.remove()
+            createGrid(size)
+            hoverGrid()
+        }
+    })
+}
+
+function removeGrid(){
+
+}
+enterButton()
+createGrid()
 hoverGrid()
